dec8c6688c932871a01b66156e9912ee10068cf3
Danil Anikin <mart4r.clearlist@gmail.com>
2025-09-28 20:14:49 +1000

    Initial Commit

---
+++ AppProjectGeneration/AppProjectGenerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppProjectGenerationApp.swift
+//  AppProjectGeneration
+//
+//  Created by Данил Аникин on 28/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppProjectGenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
